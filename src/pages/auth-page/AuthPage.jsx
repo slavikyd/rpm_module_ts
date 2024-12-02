b1e662e33ee7637b27730905ddecde7bdb66b40e
slavikyd <slavikyd@gmail.com>
2024-12-02 09:38:39 +0300

    feat: store, services, pages, layouts and components added

---
+++ src/pages/auth-page/AuthPage.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { Informer } from "@consta/uikit/Informer";
+import { useNavigate } from "react-router-dom";
+import { saveToken } from "../../services/token";
+
+const AuthPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const onFormSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const fetchUserToken = async (username, password) => {
+      const loginResponse = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          expiresInMins: 60
+        })
+      });
+
+      if (!loginResponse.ok) {
+        throw new Error("This user does not exist!");
+      }
+
+      return (await loginResponse.json()).accessToken
+    }
+
+    try {
+      saveToken(await fetchUserToken(username, password))
+      navigate("/profile")
+    } catch (error) {
+      setError(error.message)
+      return
+    }
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} style={{ width: "30vw" }}>
+
+      <div style={{ marginBottom: "16px" }}>
+
+        <label htmlFor="username" style={{ display: "block", marginBottom: "8px" }}>
+
+          Login:
+
+        </label>
+
+        <input
+          id="username"
+          type="username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+          style={{ width: "100%", padding: "8px", borderRadius: "10px" }}
+          placeholder="Enter your login"
+        />
+
+      </div>
+
+      <div style={{ marginBottom: "16px" }}>
+
+        <label htmlFor="password" style={{ display: "block", marginBottom: "8px" }}>
+
+          Password:
+        </label>
+
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          style={{ width: "100%", padding: "8px", borderRadius: "10px" }}
+          placeholder="Enter your password"
+        />
+
+      </div>
+
+      {error && (
+        <div style={{ marginBottom: "16px" }}>
+          <Informer status="alert" view="filled" title="Error" label={error} />
+        </div>
+      )}
+
+      <div style={{ display: "flex", width: "100%", justifyContent: "right" }}>
+
+        <button type="submit" style={{ backgroundColor: "white", border: "solid 2px black" }}>
+
+          Log in
+
+        </button>
+
+      </div>
+
+    </form>
+    
+  );
+};
+
+export default AuthPage;
---
+++ src/pages/servie-detail-page/ServiceDetailPage.jsx
@@ -0,0 +1,67 @@
+import { Button } from '@consta/uikit/Button';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Text } from '@consta/uikit/Text';
+import { getToken } from '../../services/token';
+
+const ServiceDetailPage = () => {
+  const [service, setService] = useState(null);
+  const { id } = useParams();
+  const [serviceId, setServiceId] = useState(id);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const userToken = getToken();
+
+    if (!userToken) {
+      navigate('/login');
+      return;
+    }
+  
+    let isNeedUpdate = true;
+
+    fetch(`https://673423afa042ab85d1190055.mockapi.io/api/v1/services/${serviceId}`, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((service) => isNeedUpdate && setService(service));
+
+    return () => {
+      isNeedUpdate = false;
+    };
+  }, [serviceId]);
+
+  const handleNextService = () => {
+    setServiceId((prevId) => {
+      const nextId = parseInt(prevId, 10) + 1;
+      return nextId.toString();
+    });
+  };
+
+  return (
+    <div style={
+      {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem'
+      }
+    }>
+      
+      <Text style={{ fontSize: '1.5rem' }}>
+        {service?.name}
+      </Text>
+      
+      <img src={service?.image} width={'500px'} style={{borderRadius:'15px'}}/>
+      
+      <Text>
+        {service?.description}
+      </Text>
+      
+      <Button onClick={handleNextService} label='Next service' />
+    
+    </div>
+  );
+};
+
+export default ServiceDetailPage;
